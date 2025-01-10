2f71fb425a270f88b6003cdca00e1fe58ae584b6
Ian Jessen <ian.jessen@ll.mit.edu>
2025-01-09 20:16:23 -0500

    tweak playback concatenationTolerance

--- internal/playback/segment_fmp4.go
+++ internal/playback/segment_fmp4.go
@@ -15,7 +15,7 @@ import (
 
 const (
 	sampleFlagIsNonSyncSample = 1 << 16
-	concatenationTolerance    = 500 * time.Millisecond
+	concatenationTolerance    = 150 * time.Millisecond
 )
 
 var errTerminated = errors.New("terminated")
